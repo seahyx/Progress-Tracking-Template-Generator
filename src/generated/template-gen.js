fb2b3fb30b185d4cd5e8456029491c353ddbf6da
Seah Ying Xiang <seahyx@gmail.com>
2023-01-01 22:21:30 +0800

    Fixed date formatting and text generation problems

--- src/generated/template-gen.js
+++ src/generated/template-gen.js
@@ -12,32 +12,37 @@ observeElement(endDateInput, "value", function () {
 });
 function onDateValueChanged() {
     if (startDateInput.value != "" && endDateInput.value != "") {
-        let startDate = new Date(startDateInput.value);
-        let endDate = new Date(endDateInput.value);
+        let startDateParts = startDateInput.value.split("/");
+        let startDate = new Date(parseInt(startDateParts[2]), parseInt(startDateParts[1]) - 1, parseInt(startDateParts[0]));
+        let endDateParts = endDateInput.value.split("/");
+        let endDate = new Date(parseInt(endDateParts[2]), parseInt(endDateParts[1]) - 1, parseInt(endDateParts[0]));
         let progressTrackingText = "";
         let tomorrow = new Date();
         iterateBetweenDates(startDate, endDate, (currentDate) => {
-            progressTrackingText += `${weekday[currentDate.getDay()]} ${currentDate.getDate().toString().padStart(2, "0")}/${currentDate
-                .getMonth()
+            progressTrackingText += `${currentDate
+                .getDate()
+                .toString()
+                .padStart(2, "0")}/${(currentDate.getMonth() + 1)
                 .toString()
                 .padStart(2, "0")}/${currentDate
                 .getFullYear()
                 .toString()
-                .slice(2)} | 0hrs |\n`;
+                .slice(2)} ${weekday[currentDate.getDay()]} | 0hrs |\n`;
             // Get tomorrow
             tomorrow.setDate(currentDate.getDate() + 1);
-            // If is end of the week
-            if (currentDate.getDay() == 1) {
-                progressTrackingText += "--- End of Week ---\n\n";
+            if (currentDate.getDay() == 0) {
+                if (tomorrow.getDate() == 1) {
+                    // End of the week and month
+                    progressTrackingText += "--- End of Week ---\n\n\n";
+                }
+                else {
+                    // If is end of the week
+                    progressTrackingText += "--- End of Week ---\n\n";
+                }
             }
             else if (tomorrow.getDate() == 1) {
                 // End of the month
                 progressTrackingText += "\n\n\n";
-            }
-            else if (currentDate.getDay() == 1 &&
-                new Date(currentDate.getDate() + 1).getDate() == 1) {
-                // End of the week and month
-                progressTrackingText += "--- End of Week ---\n\n\n";
             }
         });
         if (readBox != null) {
